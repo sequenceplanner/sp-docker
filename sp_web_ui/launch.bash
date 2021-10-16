004e1524f05914e3bf43ac2ee7979800b8d78808
Kristofer Bengtsson <kristofer@sekvensa.se>
2021-10-16 12:32:19 +0200

    upd launch files

--- sp_web_ui/launch.bash
+++ sp_web_ui/launch.bash
@@ -1,6 +1,6 @@
 # stop the screen saver
 killall light-locker
 
-# launch the ros stuff
-rviz2
-
+xrandr --newmode "1600x960_60.00"  127.00  1600 1696 1864 2128  960 963 973 996 -hsync +vsync
+xrandr --addmode VNC-0 1600x960_60.00
+xrandr --size 1280x960
--- ur_sim/launch.bash
+++ ur_sim/launch.bash
@@ -3,6 +3,10 @@
 # stop the screen saver
 killall light-locker
 
+xrandr --newmode "1600x960_60.00"  127.00  1600 1696 1864 2128  960 963 973 996 -hsync +vsync
+xrandr --addmode VNC-0 1600x960_60.00
+xrandr --size 1280x960
+
 # launch the ros stuff
 /ursim/install.sh
 /ursim/start-ursim.sh ${ROBOT_MODEL}
