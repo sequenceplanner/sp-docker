e17ae857fdfa40d2a2fdc61e33d5b9b9c8f3870f
Kristofer Bengtsson <kristofer@sekvensa.se>
2021-10-21 16:45:39 +0200

    fixed entrypoint bug

--- sp_web_ui/launch_first.bash
+++ sp_web_ui/launch_first.bash
@@ -5,7 +5,7 @@ xrandr --newmode "1600x960_60.00"  127.00  1600 1696 1864 2128  960 963 973 996 
 xrandr --newmode "1600x900_60.00"  118.25  1600 1696 1856 2112  900 903 908 934 -hsync +vsync
 xrandr --addmode VNC-0 1600x960_60.00
 xrandr --addmode VNC-0 1600x900_60.00
-xrandr --size 1280x960
+xrandr --size 1600x900
 
 if [ -f /launch.bash ]; then
   /launch.bash
